70b8fb887d7c3d4b14edd8b1c3d2faf1a2ea040a
DesmondFur <desfurtick@gmail.com>
2019-09-09 15:26:33 -0600

    created a components folder and created components

---
+++ components/Card/CardFront.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CardFront extends Component {
+    render() {
+        return (
+            <div>
+                <h1>CardFront</h1>
+            </div>
+        )
+    }
+}
+
+export default CardFront
---
+++ components/Register/SelectInterests.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SelectInterests extends Component {
+    render() {
+        return (
+            <div>
+                <h1>SelectInterests</h1>
+            </div>
+        )
+    }
+}
+
+export default SelectInterests
