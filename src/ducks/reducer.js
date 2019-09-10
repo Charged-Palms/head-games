a9c079ebd5204dd058f5f3af754479104d7b3c0e
Aaron <nordin.aaron@gmail.com>
2019-09-09 20:16:12 -0600

    Login/Register progress, redux setUser/logout

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,13 +1,49 @@
 //-----------------------------INITIAL STATE--------------------------------
 
 const initialState = {
-
-}
+  userId: null,
+  profilePic: "",
+  email: "",
+  firstName: "",
+  lastName: "",
+  gender: null,
+  age: null,
+  zipcode: null,
+  bio: "",
+  status: "",
+  topicId: null
+};
 
 //-----------------------------ACTION CONSTANTS------------------------------
 
+const SET_USER = "SET_USER";
+const LOGOUT_USER = "LOGOUT_USER";
 
 //-----------------------------ACTION BUILDERS-------------------------------
 
+export function setUser(user) {
+  return {
+    type: SET_USER,
+    payload: user
+  };
+}
+
+export function logoutUser() {
+  return {
+    type: LOGOUT_USER
+  };
+}
 
 //-----------------------------REDUCER---------------------------------------
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case LOGOUT_USER:
+      return initialState;
+    case SET_USER:
+      return { ...state, ...payload };
+    default:
+      return state;
+  }
+};
