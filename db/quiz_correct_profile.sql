ea58734cc72385a4507ba59d7ee69ab42b064ce2
Cuthbert20 <knowltonspencer@gmail.com>
2019-09-13 19:28:58 -0600

    added end point and modal

--- db/quiz_correct_profile.sql
+++ db/quiz_correct_profile.sql
@@ -1,4 +1,4 @@
-SELECT b.first_name, b.last_name, b.profile_pic, b.user_age, b.user_id, b.bio, b.status, b.zipcode, b.email, b.gender FROM user_profiles a
+SELECT b.first_name, b.last_name, b.profile_pic, b.user_age, b.user_id, b.bio, b.status, b.zipcode, b.email, b.gender, matches.match_id FROM user_profiles a
 JOIN (user_profiles b LEFT JOIN matches ON b.user_id = matches.user_id)
 ON a.user_id = matches.matchee_id
 WHERE a.user_id = $1;
