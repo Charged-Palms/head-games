4956f624bb4f01fdd72b11f90660608ef7c6dac9
DesmondFur <desfurtick@gmail.com>
2019-09-17 11:28:42 -0600

    landing tests and message styles

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import GearTest from "./components/SettingsButton/GearTest";
-import axios from "axios";
-import { getSum } from "./components/Message/Message";
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<GearTest />, div);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-describe('Examining the syntax of Jest tests', () => {
-   
-  it('sums numbers', () => {
-    expect(getSum(1,3)).toEqual(4)
-   });
-});
-
-
-
